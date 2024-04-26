7662718bba74dc8927b94f287e204b20fa690dc8
Tallal <okabi.rintaro.2001@gmail.com>
2024-04-26 22:45:41 +0200

    primer
    primer

---
+++ build/agro_mate_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tallalsudo/turtlebot3_ws/src/agro_mate/agro_mate_world
+# Build directory: /home/tallalsudo/turtlebot3_ws/src/agro_mate/build/agro_mate_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
