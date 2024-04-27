409bb9f491bca1f646fb8b05ab3665e7c6f3f92c
Alex Escriva Fuset <alfues62@gmail.com>
2024-04-27 20:40:24 +0200

    Mundo Basico
    El mundo normal no nos deja mapearlo asi que vamos a hacerlo con el basico de turtlebot3

--- build/agro_mate_world/CMakeFiles/agro_mate_world_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/agro_mate_world_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/agro_mate_world_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/agro_mate_world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tallalsudo/turtlebot3_ws/src/agro_mate/agro_mate_world
-# Build directory: /home/tallalsudo/turtlebot3_ws/src/agro_mate/build/agro_mate_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
