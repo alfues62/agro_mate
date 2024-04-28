891350a3599cbcfe96f38502656aa4dae03f0771
Tallal <okabi.rintaro.2001@gmail.com>
2024-04-28 23:58:13 +0200

    publisher
    publica en el topic initialpose

--- my_nav2_system/setup.py
+++ my_nav2_system/setup.py
@@ -1,6 +1,7 @@
+from setuptools import setup
 import os
 from glob import glob
-from setuptools import setup
+
 
 package_name = 'my_nav2_system'
 
@@ -12,19 +13,18 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'), glob('config/*.pgm')),
+         (os.path.join('share', package_name, 'config'), glob('config/*.pgm')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.lua')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'config'), glob('config/*.xml'))
-
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='afusesc',
-    maintainer_email='alfues62@gmail.com',
+    maintainer='tallalsudo',
+    maintainer_email='okabi.rintaro.2001@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
