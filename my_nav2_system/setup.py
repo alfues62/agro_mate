42228274decbe41ef927a2202cf1ed520b2e6dc2
Alex Escriva Fuset <alfues62@gmail.com>
2024-04-28 14:07:09 +0200

    No tf
    Error

--- my_nav2_system/setup.py
+++ my_nav2_system/setup.py
@@ -12,10 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.pgm')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'config'), glob('config/*.rviz')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.pgm'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.lua')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.xml'))
 
     ],
     install_requires=['setuptools'],
