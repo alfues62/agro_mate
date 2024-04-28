c1f8dff8c98832f8074b5133a47620da5a4a474a
Alex Escriva Fuset <alfues62@gmail.com>
2024-04-28 13:25:23 +0200

    Localizacion
    Colab Hecho

--- my_nav2_system/setup.py
+++ my_nav2_system/setup.py
@@ -27,6 +27,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'initial_pose_pub = my_nav2_system.initial_pose_pub:main' #añadir
         ],
     },
 )
